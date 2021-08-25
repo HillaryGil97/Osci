4ae0bf8c070001fe2166f1fbaab8b6b326458cf1
HillaryGil <hillarygil97@gmail.com>
2021-08-25 15:59:13 -0500

    activar laos canales de las señales

--- js/modules/control.js
+++ js/modules/control.js
@@ -1,9 +1,12 @@
-import {canva, gridBegin, CH2} from "./display.js"
 import { configOsci } from "./start.js"
+import {canva, gridBegin, CH2} from "./display.js"
+import {activeCH1, activeCH2} from "./channels.js"
 
 export function startMe(){ //Función incial que llama a todos los demás eventos
     if(configOsci.clickPower){
         gridBegin()
+        configOsci.botones.CH1.addEventListener('click', activeCH1)
+        configOsci.botones.CH2.addEventListener('click', activeCH2)
         //Empezando a captar los eventos de los botones y las perillas
         CH2()
     }else{
